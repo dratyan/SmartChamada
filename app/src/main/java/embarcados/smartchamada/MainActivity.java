65c2e1078ad1ba2bd3552f43cd6f065025d52d76
Airton <airtondepauli@icloud.com>
2017-07-26 20:01:14 -0300

    Servicos de Localizacao

--- app/src/main/java/embarcados/smartchamada/MainActivity.java
+++ app/src/main/java/embarcados/smartchamada/MainActivity.java
@@ -1,8 +1,20 @@
 package embarcados.smartchamada;
 
+import android.*;
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,13 +22,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.app.AlertDialog;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.zxing.Result;
+
+import java.util.Calendar;
+
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class MainActivity extends AppCompatActivity implements  ZXingScannerView.ResultHandler{
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
     private ZXingScannerView ViewScanner;
+    private TextView tView;
+    private LocationManager locManager;
+    private LocationListener locListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +44,70 @@ public class MainActivity extends AppCompatActivity implements  ZXingScannerView
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        tView = (TextView) findViewById(R.id.tView);
+
+        locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                //tView.append("\n " +location.getLatitude()+ " " + location.getLongitude());
+                location.getLatitude();
+                location.getLongitude();
+
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+
+            }
+        };
+
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET}, 10);
+                return;
+            }
+        }else {
+                locManager.requestLocationUpdates("gps", 5000, 5, locListener);
+            }
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.button);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                locManager.requestLocationUpdates("gps", 5000, 5, locListener);
                 QrScanner(view);
             }
         });
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case 10:
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    locManager.requestLocationUpdates("gps", 5000, 5, locListener);
+                return;
+
+        }
+    }
+
 
     public void QrScanner(View view){
         ViewScanner = new ZXingScannerView(this);
